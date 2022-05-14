f13ce0488ee42014cace52449590c5d6e9e35547
Upisofsht <100186303+Upisofsht@users.noreply.github.com>
2022-05-14 21:34:47 +0800

    Create main.c

---
+++ hw6/main.c
@@ -0,0 +1,85 @@
+#include <stdio.h>
+#include <stdlib.h>
+typedef struct node {
+    int data;
+    struct node* next;
+} node_t;
+node_t* allocate_node(int data) {
+    node_t* a = (node_t*)calloc(1,sizeof(node_t));
+    a->data = data;
+    a->next = NULL;
+    return a;
+}
+void show_list(node_t* list) {
+    node_t* temp = list;
+    while(temp != NULL) {
+        printf("[%d]->", temp->data);
+        temp = temp->next;
+    }
+    printf("NULL\n");
+}
+node_t* append_node(node_t* head, int new_data) {
+    node_t* node = allocate_node(new_data);
+    
+    if (head) {
+        node_t* p = head;
+        while (p->next) {
+            p = p->next;
+        }
+        p->next = node;
+    }
+    else {head = node;}
+    return head;
+}
+void free_all_node(node_t* head) {
+    node_t* tmp = NULL;
+    while (head) {
+        tmp = head;
+        head = head->next;
+        printf("free([%d])->", tmp->data);
+        free(tmp);
+    }
+    printf("NULL\n");
+}
+node_t* add_node(node_t* head, int new_deta) {
+    node_t* node = allocate_node(new_deta);
+    node->next = head;
+    return node;
+}
+node_t* del_node(node_t* head, int n) {
+    if (n == 0) {
+        node_t* next = head->next;
+        while (head->next) {
+            head->data = next->data;
+            head->next = next->next;
+            head = next;
+        }
+        return head;
+    }
+    node_t* p = head;
+    node_t* previous_node = head;
+    for (int num = 0; num < n; num++) {
+        p = p->next;
+    }
+    while (previous_node->next != p) {
+        previous_node = previous_node->next;
+    }
+    previous_node->next = p->next;
+    free(p);
+    return previous_node;
+}
+int main()
+{
+    node_t* head = NULL;
+    head = append_node(head, 0);
+    show_list(head);
+    head = append_node(head, 1);
+    show_list(head);
+    head = add_node(head, -1);
+    show_list(head);
+    head = del_node(head, 1);
+    show_list(head);
+    free_all_node(head);
+
+    return 0;
+}
